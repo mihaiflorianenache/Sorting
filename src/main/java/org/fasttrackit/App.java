263af60982853823af23f4adc8a6e8ab7feb6e52
User <mihaiflorianenache.91@gmail.com>
2019-04-06 22:16:32 +0300

    i added bubble sort method but this is not finish

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,10 +7,14 @@ public class App
     {
         int arr[] = {15,18,14,12,11,100,101,102,90,89,44,33,32,21};
 
-        //SelectionSort selectSort = new SelectionSort();
-        //selectSort.callMethodsSelectionSort(arr);
+        /*SelectionSort selectSort = new SelectionSort();
+         selectSort.callMethodsSelectionSort(arr);*/
 
-        InsertionSort insertSort=new InsertionSort();
-        insertSort.callMethodsInsertSort(arr);
+        /*InsertionSort insertSort=new InsertionSort();
+        insertSort.callMethodsInsertSort(arr);*/
+
+        BubbleSort bubbleSort=new BubbleSort();
+        bubbleSort.callMethodsBubbleSort(arr);
+
     }
 }
